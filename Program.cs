e458b2bad0abd58a268cf8f33f65810a813f149b
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-16 19:04:58 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double [,] CreateArray (int length1, int lenght2)
+{
+    double [,] Array = new double [length1, lenght2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           Array[i,j] = new Random().NextDouble();
+        }
+    }
+    return Array;
+}
+
+void PrintArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           System.Console.Write("{0,6:F2}", Array[i,j]);
+           
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(CreateArray(4, 4));
